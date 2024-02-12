07fd834c6e502f355c4fb1acf98e301c35a97a3a
SUJITH S <113872741+sujith87r@users.noreply.github.com>
2024-02-12 13:14:09 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,37 @@
+public abstract class Student implements Mark_Details,Hall_Ticket{
+    Student S=new Student() {
+        @Override
+        public int marks(int a, int b, int c) {
+            return 0;
+        }
+
+        @Override
+        public String instructions() {
+            return null;
+        }
+
+        @Override
+        public int day(int d) {
+            return 0;
+        }
+
+        @Override
+        public String name(String name) {
+            return null;
+        }
+
+        @Override
+        public double percentage(int tot, int score) {
+            return 0;
+        }
+    }
+    int tot=0;
+    @Override
+    int marks(int a,int b,int c);
+    {
+        tot=a+b+c;
+        return tot;
+    } args
+    public static void main(String [])
+
+}
