87f303174c55c71d655a1953e54fba181ec231a3
SUJITH S <113872741+sujith87r@users.noreply.github.com>
2024-02-12 13:12:42 +0530

    Create Sample1.java

---
+++ Sample1.java
@@ -0,0 +1,19 @@
+public class Sample1 {
+    Sample1()
+    {}
+
+    Sample1( int a ,int b)
+    {
+        a=10;b=10;
+    }
+    void add(int a, int b)
+    {
+        int c= a+b;
+        System.out.println("SUM :"+c);
+    }
+//    public static void main(String[] args)
+//    {
+//        Sample1 S=new Sample1(1,2);
+//        S.add(10,20);
+//    }
+}
