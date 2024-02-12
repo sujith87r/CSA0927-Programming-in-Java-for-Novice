d13454f2ef59007a429a3fb9e4dc0300b797ea59
SUJITH S <113872741+sujith87r@users.noreply.github.com>
2024-02-12 13:22:12 +0530

    Create toString().java

---
+++ toString().java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+class CGP1 {
+    public static void main (String[] args) {
+        int a=5;
+        int b=0;
+        try{
+            System.out.println(a/b);
+        }
+        catch(ArithmeticException e){
+            System.out.println(e.toString());
+        }
+    }
+}
