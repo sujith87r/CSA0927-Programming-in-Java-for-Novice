17c18b4fb457b99b88e3500270d1acdfc7e3460d
SUJITH S <113872741+sujith87r@users.noreply.github.com>
2024-02-12 13:14:38 +0530

    Create Type_Casting.java

---
+++ Type_Casting.java
@@ -0,0 +1,11 @@
+public class Type_Casting {
+    public static void main (String[] args)
+    {
+        int i;
+        double m=6.5;
+        i=(int)m;
+        System.out.println(i);
+        // String S=(String)i; int cannot be coverted to java.lang string
+       // System.out.println(i);
+    }
+}
