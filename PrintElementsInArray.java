68c75f65963a03d3c338f8bd02e1b10907d752cd
SUJITH S <113872741+sujith87r@users.noreply.github.com>
2024-02-12 13:11:25 +0530

    Create PrintElementsInArray.java

---
+++ PrintElementsInArray.java
@@ -0,0 +1,11 @@
+public class PrintElementsInArray {
+    public static void main(String[] args)
+    {
+        int a[]={5,3,2,9,6,7,4};
+        System.out.println("Finding Length : "+a.length);
+            for(int i=0;i<a.length;i++)
+            {
+                System.out.println(a[i]);
+            }
+    }
+}
