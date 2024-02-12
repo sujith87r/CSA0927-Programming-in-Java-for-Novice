f65b709e9f9d15b7d8deadf60e9f5261cb8d4dbd
SUJITH S <113872741+sujith87r@users.noreply.github.com>
2024-02-12 13:11:49 +0530

    Create PrintNNumbers.java

---
+++ PrintNNumbers.java
@@ -0,0 +1,10 @@
+public class PrintNNumbers {
+    public static void main(String[] args)
+    {
+        int n=15;
+        for(int i=1;i<=n;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
