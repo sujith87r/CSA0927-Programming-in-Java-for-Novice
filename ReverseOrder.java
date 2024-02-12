a0cbdf1f34c74a9288067f8a61bab26aad84e678
SUJITH S <113872741+sujith87r@users.noreply.github.com>
2024-02-12 13:12:11 +0530

    Create ReverseOrder.java

---
+++ ReverseOrder.java
@@ -0,0 +1,9 @@
+public class ReverseOrder {
+    public static void main(String[] args) {
+        int n = 15;
+        for (int i = n; i>0;i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
