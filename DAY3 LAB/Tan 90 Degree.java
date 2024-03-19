b42b171c718e257886d6880fc1b33053f096e1c2
SUJITH S <113872741+sujith87r@users.noreply.github.com>
2024-03-19 08:35:51 +0530

    Create Tan 90 Degree.java

---
+++ DAY3 LAB/Tan 90 Degree.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double angleInRadians = Math.toRadians(90); // Convert 90 degrees to radians
+        double tangent = Math.tan(angleInRadians);
+        System.out.println("Tan 90 degrees: " + tangent);
+    }
+}
