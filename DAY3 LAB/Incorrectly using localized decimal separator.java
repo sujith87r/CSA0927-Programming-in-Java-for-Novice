551c7970b13a9ddf2216d8616b12580fe8abc844
SUJITH S <113872741+sujith87r@users.noreply.github.com>
2024-03-19 08:40:06 +0530

    Create Incorrectly using localized decimal separator.java

---
+++ DAY3 LAB/Incorrectly using localized decimal separator.java
@@ -0,0 +1,2 @@
+String value = "1.23"; // Using a period (.) as the decimal separator
+double parsedValue = Double.parseDouble(value); // Incorrect usage in a locale where comma (,) is the decimal separator
