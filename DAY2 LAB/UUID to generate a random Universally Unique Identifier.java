e01d54931418ef1776a4a1737394a9e2c81fa769
SUJITH S <113872741+sujith87r@users.noreply.github.com>
2024-03-19 08:32:06 +0530

    Create UUID to generate a random Universally Unique Identifier.java

---
+++ DAY2 LAB/UUID to generate a random Universally Unique Identifier.java
@@ -0,0 +1,9 @@
+import java.util.UUID;
+public class UUIDExample {
+ public static void main(String[] args) {
+ // Generating a random UUID
+ UUID uuid = UUID.randomUUID();
+ // Displaying the generated UUID
+ System.out.println("Random UUID: " + uuid);
+ }
+}
